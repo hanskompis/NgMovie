8aa3e9504852eec90a825ddc423a1447ed319043
Hansi Keijonen <renejacquesfury@gmail.com>
2013-11-16 17:51:28 +0200

    changed controller location

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,22 @@ angular.module('ngMovieApp', [
       .otherwise({
         redirectTo: '/'
       });
+  }])
+  .config(['$httpProvider', function ($httpProvider) {
+    
+      $httpProvider.defaults.useXDomain = true;
+      $httpProvider.defaults.headers.get = {'Accept':'application/json'};
+      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  }])
+
+    .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+
+      $http.get('http://www.omdbapi.com/?s=Love')
+        
+        .success(function(data) {
+
+          $scope.data = data["Search"];
+        });
   }]);
 
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,18 +3,20 @@
 angular.module('ngMovieApp')
 
 
-    .config(['$httpProvider', function ($httpProvider) {
+ /*   .config(['$httpProvider', function ($httpProvider) {
     
       $httpProvider.defaults.useXDomain = true;
       $httpProvider.defaults.headers.get = {'Accept':'application/json'};
       delete $httpProvider.defaults.headers.common['X-Requested-With'];
   }])
 
-  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+    .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
-  	  $http.get('http://www.omdbapi.com/?s=Love').success(function(data) {
- 
+  	  $http.get('http://www.omdbapi.com/?s=Love')
+  	    
+  	    .success(function(data) {
+
           $scope.data = data["Search"];
-      });
- 
+        });
   }]);
+*/
