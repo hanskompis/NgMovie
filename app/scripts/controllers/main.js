160bc17d7fb48d780f9326ba44556b73f1849fcf
Hansi Keijonen <renejacquesfury@gmail.com>
2013-11-16 16:53:26 +0200

    cors added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,16 @@
 'use strict';
 
 angular.module('ngMovieApp')
+
+  .config(['$httpProvider', function ($httpProvider) {
+
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  }])
+
   .controller('MainCtrl', function ($scope, $http) {
-  	  $http.get('manywars.json').success(function(data) {
+
+  	  $http.get('http://www.omdbapi.com/?s=Star').success(function(data) {
+ 
           $scope.data = data["Search"];
       });
  
