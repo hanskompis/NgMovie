6ffa4b7bf50ab66d916345c123caeb4c98f98424
Hansi Keijonen <renejacquesfury@gmail.com>
2013-11-17 23:07:09 +0200

    added query form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,45 @@
 'use strict';
 
-angular.module('ngMovieApp')
+var app = angular.module('ngMovieApp')
 
 
-    .config(['$httpProvider', function ($httpProvider) {
+app.config(['$httpProvider', function ($httpProvider) {
     
-      $httpProvider.defaults.useXDomain = true;
-      $httpProvider.defaults.headers.get = {'Accept':'application/json'};
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  }])
+  $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.headers.get = {'Accept':'application/json'};
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
 
+app.factory('Movie', function ($http) {
+  
+  var movie = {};
+  
+  movie.query = function (queryString) {
+    return  $http.get('http://www.omdbapi.com/?s='+queryString);
+  }
+
+    return movie;
+});
+
+app.controller('MainCtrl',  function ($scope, Movie) {
+
+  $scope.movieName = '';
+  
+  var queryForMovie = function () {
+    Movie.query($scope.movieName)
+        
+    .success(function (data) {
+      $scope.data = data["Search"];
+    });  
+  };
+
+  $scope.search = function () {
+    queryForMovie();      
+  }
+
+  });
+
+/*
     .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
   	  $http.get('http://www.omdbapi.com/?s=Love')
@@ -19,3 +49,5 @@ angular.module('ngMovieApp')
           $scope.data = data["Search"];
         });
   }]);
+
+*/
